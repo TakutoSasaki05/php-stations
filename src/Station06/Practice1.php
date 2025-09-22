c2fa669a753dfa9f72c9a2d2dcd7b2d5527261f3
takutaku <takuto.cocoa1911@gmail.com>
2025-09-22 16:40:37 +0900

    from win

--- src/Station06/Practice1.php
+++ src/Station06/Practice1.php
@@ -6,7 +6,15 @@ class Practice1
 {
     public function main(): void
     {
-        // ここにサンプルコードを記述
+        // 初期の配列
+        $array = ['red', 'blue', 'yellow'] ;
+        // インデックス 0 に 'white' , インデックス 1 に 'black' を追加
+        array_unshift($array, 'white', 'black');
+        // 要素 'yellow' を削除
+        array_pop($array);
+        // 要素 'red' と 'blue' の間に 'green' を挿入
+        array_splice($array,3,0,'green');
+        print_r($array);
     }
 }
 
