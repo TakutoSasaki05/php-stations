5160502f4b7ac9d4f6ec231804b1706216c619cc
takutaku <takuto.cocoa1911@gmail.com>
2025-09-25 17:52:49 +0900

    課題9途中

--- src/Station09/Question.php
+++ src/Station09/Question.php
@@ -6,5 +6,34 @@ class Question
 {
     public function main(): array
     {
+        $users = [
+            [
+                'id' => 1,
+                'last_name' => '山田',
+                'first_name' => '太郎',
+                'age' => 20,
+                'password' => 'yamada',
+            ],
+            [
+                'id' => 2,
+                'last_name' => '鈴木',
+                'first_name' => '次郎',
+                'password' => 'suzuki',
+            ],
+            [
+                'id' => 3,
+                'last_name' => '佐藤',
+                'first_name' => '花子',
+                'password' => 'sato',
+            ],
+        ];
+        $full_names = array_map(function ($user) {
+            return $user['last_name'] . $user['first_name'];
+        }, $users);
+        foreach ($array as $value) {
+            $full_names = array_map(function ($user) {
+                return $user['last_name'] . $user['first_name'];
+            }, $users);
+        }
     }
 }
