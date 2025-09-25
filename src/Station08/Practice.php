a5424db05c44f6131cef1e197504040cd4b90eb3
takutaku <takuto.cocoa1911@gmail.com>
2025-09-25 17:13:21 +0900

    課題8

--- src/Station08/Practice.php
+++ src/Station08/Practice.php
@@ -7,6 +7,16 @@ class Practice
     public function main(): void
     {
         // ここにサンプルコードを記述
+        $array = [
+            ['アザラシ','アライグマ'],
+            ['ウサギ','ウシ','ウマ'],
+            ['オオカミ','オットセイ']
+        ];
+        print_r($array);
+        array_splice($array[2],0,2,$array[1]);
+        print_r($array);
+        array_splice($array[1],0,3,['イヌ','イルカ']);
+        print_r($array);
     }
 }
 
--- src/Station08/Question.php
+++ src/Station08/Question.php
@@ -6,5 +6,13 @@ class Question
 {
     public function main(): array
     {
+        $array = [
+            ['アザラシ','アライグマ'],
+            ['ウサギ','ウシ','ウマ'],
+            ['オオカミ','オットセイ']
+        ];
+        array_splice($array[2],0,2,$array[1]);
+        array_splice($array[1],0,3,['イヌ','イルカ']);
+        return $array;
     }
 }
