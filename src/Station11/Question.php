e46e83f15fe74b0cbfdb2527328fa23373836aa8
takutaku <takuto.cocoa1911@gmail.com>
2025-09-30 13:39:33 +0900

    課題11途中

--- src/Station11/Question.php
+++ src/Station11/Question.php
@@ -6,17 +6,39 @@ class Question
 {
     public function main(array $sweets): array
     {
+        $budget = 300;
+        $lessThanBudgetSweets = getSweetsLessThanBudget($sweets, $budget);
+        $key = getRandomKeys($lessThanBudgetSweets, $budget);
+        $combination = makeCombination($lessThanBudgetSweets, $key);
+        return $combination;
     }
 
     private function getSweetsLessThanBudget(array $sweets, int $budget)
     {
+        $lessThanBudgetSweets = array_filter(function ($sweets) {
+            return $sweets['price'] <= $budget;
+        }, $sweets);
+        return $lessThanBudgetSweets;
+
     }
 
     private function getRandomKeys(array $lessThanBudgetSweets, int $budget)
     {
+        $keys = array_rand($lessThanBudgetSweets, 3);
+        return $keys;
     }
 
     private function makeCombination(array $sweets, array $keys)
     {
+        $combination = [];
+        $totalPrice = 0;
+        foreach ($keys as $key) {
+            $combination[] = $sweets[$key];
+            $totalPrice += $sweets[$key]['price'];
+        }
+        if ($totalPrice > 300) {
+            return $this->main($sweets);
+        }
+        return $combination;
     }
 }
